bbe0f4afbc14079db5ea589a401cd2b67142950c
Lars Selstad <lars.selstad@norskluftambulanse.no>
2016-01-21 12:52:21 +0100

    Nytt forsøk med config og deploy script v2

--- deploy-to-beta.sh
+++ deploy-to-beta.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-echo "Running deploy script"
+echo -e "Running deploy script"
 
-echo "On branch: ${TRAVIS_BRANCH}"
+echo -e "On branch: ${TRAVIS_BRANCH}"
 
-echo "Is PR: ${TRAVIS_PULL_REQUEST}"
+echo -e "Is PR: ${TRAVIS_PULL_REQUEST}"
 
-if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  echo "Deploying to beta server"
+if [ "${TRAVIS_BRANCH}" == "dev" ] && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+  echo -e "Deploying to beta server"
   chmod 600 deploy_key
   mv deploy_key ~/.ssh/id_rsa
   git remote add deploy ssh://ci@178.62.103.161/var/repo/met-radar-test-beta.git
   git push deploy dev
+  echo -e "Deploy done"
 else
-  echo "This will not deploy!"
-  echo "The branch is not dev or it is a PR"
+  echo -e "This will not deploy!"
+  echo -e "The branch is not dev or it is a PR"
 fi
