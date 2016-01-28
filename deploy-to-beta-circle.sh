fbba133e7c8ab45a16f3b5229b29e0e2d1036888
Lars Selstad <lars.selstad@norskluftambulanse.no>
2016-01-28 13:01:59 +0100

    La tilbake git push i deploy script

--- deploy-to-beta-circle.sh
+++ deploy-to-beta-circle.sh
@@ -7,4 +7,7 @@ echo -e "Is PR: $CI_PULL_REQUEST"
 
 echo -e "Deploying to beta server"
 
+git remote add deploy ssh://ci@178.62.103.161/var/repo/met-radar-test-beta.git
+git push deploy dev
+
 echo -e "Deploy done"
